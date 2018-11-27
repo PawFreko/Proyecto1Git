736e0ebbbc9675a350f35520c617f6178b709b38
PawFreko <pawyfreko@gmail.com>
2018-11-27 17:42:42 -0600

    Mi segundo commit de proyecto JAV!!

---
+++ src/com/PruebaGit/com/controller/MesajeEmpleadoServlet.java
@@ -0,0 +1,32 @@
+package com.PruebaGit.com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MesajeEmpleadoServlet
+ */
+@WebServlet("/MesajeEmpleadoServlet")
+public class MesajeEmpleadoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MesajeEmpleadoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/PruebaGit/com/modelos/empleados/Empleados.java
@@ -0,0 +1,18 @@
+package com.PruebaGit.com.modelos.empleados;
+
+public class Empleados {
+
+	
+	private String idEmpleado;
+
+	public String getIdEmpleado() {
+		return idEmpleado;
+	}
+
+	public void setIdEmpleado(String idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+
+	}
+
+
